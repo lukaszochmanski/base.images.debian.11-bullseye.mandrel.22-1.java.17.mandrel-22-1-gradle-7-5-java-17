0667812111a8ec9541b465a55f3783dab6a37c70
Lukasz Ochmanski <github@ochmanski.de>
2022-10-05 19:30:47 +0200

    Update 08-install-gradle.sh

--- scripts/install/08-install-gradle.sh
+++ scripts/install/08-install-gradle.sh
@@ -3,9 +3,9 @@
 set +x
 set -e
 
-curl https://downloads.gradle.org/distributions/gradle-7.4-bin.zip > gradle.zip
+curl https://downloads.gradle.org/distributions/gradle-7.5-bin.zip > gradle.zip
 unzip -d /opt gradle.zip
-mv /opt/gradle-7.4 /opt/gradle
+mv /opt/gradle-7.5 /opt/gradle
 ln -s /opt/gradle/bin/gradle /usr/bin/gradle
 rm gradle.zip
 
