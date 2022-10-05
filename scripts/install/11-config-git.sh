3e081fb60341a85072d2961c66b1282bc27af91f
Lukasz Ochmanski <github@ochmanski.de>
2022-10-05 19:30:18 +0200

    Update 11-config-git.sh

--- scripts/install/11-config-git.sh
+++ scripts/install/11-config-git.sh
@@ -4,7 +4,7 @@ set +x
 set -e
 
 git config --global user.name "AWS CodeBuild" \
-    && git config --global user.email "code.build@blueid.net" \
+    && git config --global user.email "your@domain.net" \
     && git config --global pager.branch false \
     && git config --global pager.tag false \
     && git config --global init.defaultBranch main
